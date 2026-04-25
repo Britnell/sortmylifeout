f362d2604900b54eec0e3e59730035bcb6d98c93
Britnell <tommy.britnell@gmail.com>
2026-04-25 18:24:47 +0200

    fix schema betAuth

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -3,46 +3,46 @@ CREATE TABLE IF NOT EXISTS "user" (
   id TEXT PRIMARY KEY,
   name TEXT NOT NULL,
   email TEXT UNIQUE NOT NULL,
-  email_verified INTEGER NOT NULL DEFAULT 0,
+  emailVerified INTEGER NOT NULL DEFAULT 0,
   image TEXT,
-  created_at INTEGER NOT NULL,
-  updated_at INTEGER NOT NULL
+  createdAt INTEGER NOT NULL,
+  updatedAt INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "session" (
   id TEXT PRIMARY KEY,
-  expires_at INTEGER NOT NULL,
+  expiresAt INTEGER NOT NULL,
   token TEXT UNIQUE NOT NULL,
-  created_at INTEGER NOT NULL,
-  updated_at INTEGER NOT NULL,
-  ip_address TEXT,
-  user_agent TEXT,
-  user_id TEXT NOT NULL REFERENCES "user"(id) ON DELETE CASCADE
+  createdAt INTEGER NOT NULL,
+  updatedAt INTEGER NOT NULL,
+  ipAddress TEXT,
+  userAgent TEXT,
+  userId TEXT NOT NULL REFERENCES "user"(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS "account" (
   id TEXT PRIMARY KEY,
-  account_id TEXT NOT NULL,
-  provider_id TEXT NOT NULL,
-  user_id TEXT NOT NULL REFERENCES "user"(id) ON DELETE CASCADE,
-  access_token TEXT,
-  refresh_token TEXT,
-  id_token TEXT,
-  access_token_expires_at INTEGER,
-  refresh_token_expires_at INTEGER,
+  accountId TEXT NOT NULL,
+  providerId TEXT NOT NULL,
+  userId TEXT NOT NULL REFERENCES "user"(id) ON DELETE CASCADE,
+  accessToken TEXT,
+  refreshToken TEXT,
+  idToken TEXT,
+  accessTokenExpiresAt INTEGER,
+  refreshTokenExpiresAt INTEGER,
   scope TEXT,
   password TEXT,
-  created_at INTEGER NOT NULL,
-  updated_at INTEGER NOT NULL
+  createdAt INTEGER NOT NULL,
+  updatedAt INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "verification" (
   id TEXT PRIMARY KEY,
   identifier TEXT NOT NULL,
   value TEXT NOT NULL,
-  expires_at INTEGER NOT NULL,
-  created_at INTEGER,
-  updated_at INTEGER
+  expiresAt INTEGER NOT NULL,
+  createdAt INTEGER,
+  updatedAt INTEGER
 );
 
 -- App tables
