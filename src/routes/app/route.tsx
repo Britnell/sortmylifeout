7965801a3ae2b46e9329ed422771592db6d95dc1
Britnell <tommy.britnell@gmail.com>
2026-04-25 17:23:46 +0200

    check login

--- src/routes/app/route.tsx
+++ src/routes/app/route.tsx
@@ -1,10 +1,20 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router'
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
+import { getSessionFn } from '../../serverFn/queries.functions'
 
 export const Route = createFileRoute('/app')({
+  beforeLoad: async () => {
+    const userId = await getSessionFn()
+    if (!userId) {
+      throw redirect({ to: '/login' })
+    }
+    console.log({ userId })
+    return { userId }
+  },
   component: RouteComponent,
 })
 
 function RouteComponent() {
+
   return (
     <div>
       <header>App</header>
--- src/serverFn/queries.functions.ts
+++ src/serverFn/queries.functions.ts
@@ -1,24 +1,31 @@
 import { createServerFn } from '@tanstack/react-start'
 import { getRequest } from '@tanstack/react-start/server'
 import { loginUser, signupUser, logoutUser } from '../lib/auth.server'
+import { getSession } from '../lib/auth'
 
 export const loginFn = createServerFn({ method: 'POST' })
-	.inputValidator((d: { username: string; password: string }) => d)
-	.handler(async ({ data }) => {
-		return loginUser(data.username, data.password)
-	})
+  .inputValidator((d: { username: string; password: string }) => d)
+  .handler(async ({ data }) => {
+    return loginUser(data.username, data.password)
+  })
 
 export const signupFn = createServerFn({ method: 'POST' })
-	.inputValidator(
-		(d: { username: string; email: string; password: string }) => d,
-	)
-	.handler(async ({ data }) => {
-		return signupUser(data.username, data.email, data.password)
-	})
+  .inputValidator(
+    (d: { username: string; email: string; password: string }) => d,
+  )
+  .handler(async ({ data }) => {
+    return signupUser(data.username, data.email, data.password)
+  })
 
-export const logoutFn = createServerFn({ method: 'POST' }).handler(
-	async () => {
-		const request = getRequest()
-		return logoutUser(request)
-	},
+export const logoutFn = createServerFn({ method: 'POST' }).handler(async () => {
+  const request = getRequest()
+  return logoutUser(request)
+})
+
+export const getSessionFn = createServerFn({ method: 'GET' }).handler(
+  async () => {
+    const request = getRequest()
+    const session = await getSession(request)
+    return session?.userId ?? null
+  },
 )
